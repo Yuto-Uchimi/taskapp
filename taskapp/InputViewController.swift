cc49953c084f5fb5aeaef1bfa53c37d98ea667ef
Yuto-Uchimi <kingdom785@icloud.com>
2019-10-26 19:03:34 +0900

    Initial Commit

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class InputViewController: UIViewController {
+    
+    @IBOutlet weak var titleTaxtField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
